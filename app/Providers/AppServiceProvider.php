c6e8f690b2be97c1b1ab3aa0d3e15f673ac9c175
Qurban Ullah <qurbanullah@avouch.org>
2022-06-16 07:10:48 +0500

    app service providor address fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // // Let laravel know the public path of your application
+        if (env('APP_URL') == "https://iwill.pk"){
+            $this->app->bind('path.public', function () {
+                return base_path('../public_html');
+            // Change httpdocs to public_html if you are using cpanel
+            });
+        }
     }
 
     /**
